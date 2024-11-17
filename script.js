7a33f1e432d3c399211ba2cb76afca12f415145b
j-99j <jaakub.grolik@o2.pl>
2024-11-17 16:02:57 +0100

    Update 1.4.1 fix #1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const modal = document.getElementById("imageModal");
     const modalImg = document.getElementById("modalImg");
     const captionText = document.getElementById("caption");
@@ -6,39 +6,46 @@ document.addEventListener("DOMContentLoaded", function() {
     let images = [];
     let currentImageIndex = 0;
 
-    document.querySelectorAll(".project img").forEach(img => {
-        img.addEventListener("click", function() {
+    document.querySelectorAll(".project img").forEach((img) => {
+        img.addEventListener("click", function () {
             images = this.getAttribute("data-images").split(", ");
             currentImageIndex = 0;
             showImage();
 
-            modal.style.display = "block";
+            modal.style.display = "flex";
             captionText.innerHTML = this.alt;
+            document.body.classList.add("modal-open");
         });
     });
 
-    closeBtn.onclick = function() {
-        modal.style.display = "none";
+    closeBtn.onclick = function () {
+        closeModal();
+    };
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    };
+
+    document.getElementById("next").onclick = function () {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        showImage();
+    };
+
+    document.getElementById("prev").onclick = function () {
+        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+        showImage();
     };
 
     function showImage() {
         modalImg.src = images[currentImageIndex];
         modalImg.style.maxHeight = "80vh";
+        modalImg.style.maxWidth = "90vw";
     }
 
-    document.getElementById("next").onclick = function() {
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        showImage();
-    };
-
-    document.getElementById("prev").onclick = function() {
-        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-        showImage();
-    };
-
-    window.onclick = function(event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    };
+    function closeModal() {
+        modal.style.display = "none";
+        document.body.classList.remove("modal-open");
+    }
 });
